19a169a41faa795221aa9060dc2e12de0f4b2e8c
Lsp <spyro9282@gmail.com>
2022-09-20 18:06:15 +0900

    html commit

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://pyo:jk1jk2jk3@cluster0.nygwmem.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
 
-
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
 
 
 
