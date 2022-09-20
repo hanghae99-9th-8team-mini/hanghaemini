f1f8ab40893a8bf386daf2e37339622cd328597e
Lsp <spyro9282@gmail.com>
2022-09-20 22:00:49 +0900

    url upload api commit

--- app.py
+++ app.py
@@ -10,10 +10,20 @@ db = client.dbsparta
 
 
 @app.route('/')
-def home():
+def main():
     return render_template('index.html')
 
-
+@app.route('/main', methods=['POST'])
+def upload_music():
+    url_receive = request.form['url_give']
+    genre_receive = request.form['genre_give']
+    print(url_receive,genre_receive)
+    doc = {
+        'url': url_receive,
+        'genre': genre_receive
+    }
+    # db.테이블.insert_one(doc)
+    return jsonify({'msg': '등록완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
