79c0aa85a8092b952a101a56b64fd03d224a8c59
leejun <koreadlwns@naver.com>
2022-09-21 23:46:23 +0900

    third commit

--- miniproject_login/app.py
+++ miniproject_login/app.py
@@ -4,18 +4,20 @@ import jwt
 import datetime
 import hashlib
 from flask import Flask, render_template, jsonify, request, redirect, url_for
+import requests
+from bs4 import BeautifulSoup
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
-
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
-SECRET_KEY = 'SPARTA'
+SECRET_KEY = 'secret'
 
 ca = certifi.where()
-client = MongoClient('mongodb+srv://test:sparta@cluster0.gfgemqc.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('mongodb+srv://test:sparta@cluster0.gfgemqc.mongodb.net/?retryWrites=true&w=majority',
+                     tlsCAFile=ca)
 db = client.dbsparta
 
 
@@ -23,6 +25,23 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
+
+@app.route('/home')
+def home_login():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.minilogin.find_one({"username": payload["id"]})
+        return render_template('home.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("home", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("home", msg="로그인 정보가 존재하지 않습니다."))
+
+
+@app.route('/select')
+def select():
+    return render_template('select.html')
 
 
 @app.route('/login')
@@ -56,8 +75,8 @@ def sign_in():
 
     if result is not None:
         payload = {
-         'id': username_receive,
-         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
@@ -74,12 +93,12 @@ def sign_up():
     nickname_receive = request.form['nickname_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
-        "username": username_receive,                               # 아이디
-        "password": password_hash,                                  # 비밀번호
-        "profile_name": nickname_receive,                           # 프로필 이름 기본값은 닉네임
-        "profile_pic": "",                                          # 프로필 사진 파일 이름
-        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
-        "profile_info": ""                                          # 프로필 한 마디
+        "username": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+        "profile_name": nickname_receive,  # 프로필 이름 기본값은 닉네임
+        "profile_pic": "",  # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png",  # 프로필 사진 기본 이미지
+        "profile_info": ""  # 프로필 한 마디
     }
     db.minilogin.insert_one(doc)
     return jsonify({'result': 'success'})
@@ -98,10 +117,10 @@ def save_img():
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         username = payload["id"]
-        name_receive = request.form["name_give"]
+        nickname_receive = request.form["nickname_give"]
         about_receive = request.form["about_give"]
         new_doc = {
-            "profile_name": name_receive,
+            "profile_name": nickname_receive,
             "profile_info": about_receive
         }
         if 'file_give' in request.files:
@@ -109,58 +128,67 @@ def save_img():
             filename = secure_filename(file.filename)
             extension = filename.split(".")[-1]
             file_path = f"profile_pics/{username}.{extension}"
-            file.save("./static/"+file_path)
+            file.save("./static/" + file_path)
             new_doc["profile_pic"] = filename
             new_doc["profile_pic_real"] = file_path
-        db.minilogin.update_one({'username': payload['id']}, {'$set':new_doc})
+        db.minilogin.update_one({'username': payload['id']}, {'$set': new_doc})
         return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
 
 @app.route('/posting', methods=['POST'])
-def posting():
+def music_posting():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         user_info = db.minilogin.find_one({"username": payload["id"]})
-        comment_receive = request.form["comment_give"]
-        date_receive = request.form["date_give"]
-        doc = {
-            "username": user_info["username"],
-            "profile_name": user_info["profile_name"],
-            "profile_pic_real": user_info["profile_pic_real"],
-            "comment": comment_receive,
-            "date": date_receive
-        }
-        db.miniposts.insert_one(doc)
+        url_receive = request.form['url_give']
+        result = db.minisong.find_one({"melon_url": url_receive})
+        if result is None:
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+            data = requests.get(url_receive, headers=headers)
 
-        return jsonify({"result": "success", 'msg': '포스팅 성공'})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
+            soup = BeautifulSoup(data.text, 'html.parser')
 
+            div = soup.select_one('#downloadfrm > div > div > div.entry')
+            title = div.select_one('div.info > div.song_name').get_text(strip=True).lstrip("곡명")
+            artist = div.select_one('div.artist').text
+            # downloadfrm > div > div > div.entry > div.info > div.artist
+            album_title = div.select_one('div.meta > dl > dd:nth-child(2) > a').get_text()
+            album_img = soup.select_one('#downloadfrm > div > div > div.thumb > a > img')["src"]
+            genre = div.select_one('div.meta > dl > dd:nth-child(6)').get_text()
 
-@app.route("/get_posts", methods=['GET'])
-def get_posts():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        username_receive = request.args.get("username_give")
-        if username_receive == "":
-            posts = list(db.miniposts.find({}).sort("date", -1).limit(20))
+            print(title, artist, album_title, album_img, genre)
+
+            doc = {
+                "username": user_info["username"],
+                "profile_name": user_info["profile_name"],
+                "profile_pic_real": user_info["profile_pic_real"],
+                'melon_url': url_receive,
+                'title': title,
+                'artist': artist,
+                'album_title': album_title,
+                'album_img': album_img,
+                'genre': genre
+            }
+            db.minisong.insert_one(doc)
+            return jsonify({"result": "success", 'msg': '포스팅 성공'})
         else:
-            posts = list(db.miniposts.find({"username": username_receive}).sort("date", -1).limit(20))
-        for post in posts:
-            post["_id"] = str(post["_id"])
-            post["count_heart"] = db.minilikes.count_documents({"post_id": post["_id"], "type": "heart"})
-            post["heart_by_me"] = bool(db.minilikes.find_one({"post_id": post["_id"], "type": "heart", "username": payload['id']}))
-            post["count_star"] = db.minilikes.count_documents({"post_id": post["_id"], "type": "star"})
-            post["star_by_me"] = bool(db.minilikes.find_one({"post_id": post["_id"], "type": "star", "username": payload['id']}))
-            post["count_like"] = db.minilikes.count_documents({"post_id": post["_id"], "type": "like"})
-            post["like_by_me"] = bool(db.minilikes.find_one({"post_id": post["_id"], "type": "like", "username": payload['id']}))
-        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", "posts": posts})
+            return jsonify({'msg': '이미 등록된 URL입니다!'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
+
+
+@app.route("/posting", methods=['GET'])
+def get_posts():
+    username_receive = request.args.get("username_give")
+    if username_receive == "":
+        posts = list(db.minisong.find({}, {'_id': False}))
+    else:
+        posts = list(db.minisong.find({"username": username_receive}, {'_id': False}))
+    return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", "posts": posts})
 
 
 @app.route('/update_like', methods=['POST'])
---
+++ miniproject_login/static/script.js
@@ -0,0 +1,83 @@
+        function login() {
+            window.location.href = "/login"
+        }
+
+        function logout() {
+            $.removeCookie('mytoken');
+            alert('로그아웃!')
+            window.location.href = '/'
+        }
+
+        $(document).ready(function () {
+            listing()
+
+            $('.genre-div-k').hide();
+            $('.genre-div-p').hide();
+
+            $('#k-toggle').click(function () {
+                $('.genre-div-p').hide();
+                $('.genre-div-k').show();
+            });
+
+            $('#p-toggle').click(function () {
+                $('.genre-div-k').hide();
+                $('.genre-div-p').show();
+            });
+        })
+
+        function post() {
+            let url = $('#url-input').val()
+            let genre = $('#genre-input').val()
+            if (url == "") {
+                alert("URL을 입력하세요!")
+            } else {
+                $.ajax({
+                    type: 'POST',
+                    url: '/posting',
+                    data: {url_give: url},
+                    success: function (response) {
+                        if (response['msg'] == '이미 등록된 URL입니다!') {
+                            alert(response['msg'])
+                        } else {
+                            alert(response['msg'])
+                            window.location.reload()
+                        }
+                    }
+                });
+            }
+        }
+
+        function listing(username) {
+            if (username == undefined) {
+                username = ""
+            }
+            $('#card-div').empty()
+            $.ajax({
+                type: 'GET',
+                url: `/posting?username_give=${username}`,
+                data: {},
+                success: function (response) {
+                    let rows = response['posts']
+                    for (let i = 0; i < rows.length; i++) {
+                        let title = rows[i]['title']
+                        let artist = rows[i]['artist']
+                        let album_title = rows[i]['album_title']
+                        let album_img = rows[i]['album_img']
+                        let genre = rows[i]['genre']
+
+                        console.log(title, artist, album_img, album_title, genre)
+                        let temp_html = `<div class="album-card" onclick="music_info()">
+                                            <img src="${album_img}"
+                                                 class="album-img">
+                                            <div class="album-item">
+                                                <span class="music-title">${title}</span>
+                                                <span class="music-artist">${artist}</span>
+                                                <span class="album-title">${album_title}</span>
+                                                <span class="album-genre" id="genre">${genre}</span>
+                                            </div>
+                                        </div>`
+                        $('#card-div').append(temp_html)
+                    }
+                }
+            });
+        }
